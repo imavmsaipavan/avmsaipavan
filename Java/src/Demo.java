e2d450aad2da08aa56c3c768e7c7caa9d84c3202
mani <mani@Administrator>
2021-03-06 01:43:59 +0530

    for loop added

--- Java/src/Demo.java
+++ Java/src/Demo.java
@@ -2,13 +2,13 @@
 public class Demo {
 
 	public static void main(String[] args) {
-System.out.println("Super is going ");
-System.out.println("Super is going ");
-System.out.println("Super is going ");
-System.out.println("Super is going ");
-System.out.println("Super is going ");
-System.out.println("Super is going ");
 
+		System.out.println("Super is going ");
+		System.out.println("Super is going ");
+
+for(int i=0;i<10;i++) {
+	System.out.println(i);
+}
 	}
 
 }
