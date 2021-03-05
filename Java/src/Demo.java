62df9e7076b1e30a88e61e5b66cae550e3274901
mani <mani@Administrator>
2021-03-05 21:48:27 +0530

    sjfjadsndklaslk

--- Java/src/Demo.java
+++ Java/src/Demo.java
@@ -3,6 +3,10 @@ public class Demo {
 
 	public static void main(String[] args) {
 System.out.println("Super is going ");
+System.out.println("Super is going ");
+System.out.println("Super is going ");
+System.out.println("Super is going ");
+
 	}
 
 }
