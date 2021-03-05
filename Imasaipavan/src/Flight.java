bc8a0080d60e5511782916cf1f91c1b63c100f24
mani <mani@Administrator>
2021-03-06 00:56:30 +0530

    new String added

--- Imasaipavan/src/Flight.java
+++ Imasaipavan/src/Flight.java
@@ -3,6 +3,8 @@ public class Flight {
 
 	public static void main(String[] args) {
 System.out.println("you achieved what you want");
+String s="new String is added";
+System.out.println(s);
 	}
 
 }
